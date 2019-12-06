ef3549f2a50b0273e2a0623475caf691995c75f1
James <jinglis12@googlemail.com>
2019-12-06 18:30:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data[:gender].each_value { |e| e.map { |el| new_hash[el] = {}}}
+
+  data.each { |k, v| v.each { |key, value| 
+      value.map { |e| 
+        if new_hash[e][k] && !new_hash[e][k].include?(key.to_s)
+          new_hash[e][k].push(key.to_s)
+        else
+          new_hash[e][k] = [key.to_s]
+        end
+      }
+    }}
+  new_hash
 end
